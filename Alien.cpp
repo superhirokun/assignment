5a5a0dcb636d7f06ac72171c4e7c7868416a1c9d
Yap Choo Kath Moon <1211102270@student.mmu.edu.my>
2023-02-01 21:40:27 +0800

    Alien.cpp
      tempCodeRunnerFile.cpp

--- Alien.cpp
+++ Alien.cpp
@@ -2,7 +2,7 @@ using namespace std;
 
 class alien: public game{
     private:
-    vector< vector <char> > map;
+    vector< vector <char> > amap;
     int dx, dy;
     public:
     int x , y;
@@ -18,7 +18,7 @@ void alien::centre(int x, int y)
     dx =x ; dy = y;
     int centreX = x/2;
     int centreY = y/2;
-    map[centreX][centreY] = 'A';
+    amap[centreX][centreY] = 'A';
 }
 
 void alien::move(int direction)
@@ -30,7 +30,7 @@ void alien::move(int direction)
     {
         for (int k = 0; k < x && !found; ++k)
         {
-            if (map[i][k] == 'A')
+            if (amap[i][k] == 'A')
             {
                 x = i;
                 y = k;
@@ -41,23 +41,23 @@ void alien::move(int direction)
     //Update the position of the alien based on the direction
     switch(direction){
         case 'U':
-            map[x][y] = '.';
-            map[x][y+1] = 'A';
+            amap[x][y] = '.';
+            amap[x][y+1] = 'A';
             break;
 
         case 'D':
-            map[x][y] = '.';
-            map[x][y-1] = 'A';
+            amap[x][y] = '.';
+            amap[x][y-1] = 'A';
             break;
 
         case 'R':
-            map[x][y] = '.';
-            map[x+1][y] = 'A';
+            amap[x][y] = '.';
+            amap[x+1][y] = 'A';
             break;
 
         case 'L':
-            map[x][y] = '.';
-            map[x-1][y] = 'A';
+            amap[x][y] = '.';
+            amap[x-1][y] = 'A';
             break;
 
     }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include "include.h"
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int x, y; char c;
+    cout << "PLease entre the row of your selection = " ;
+    cin >> x;
+    cout << "Please entre the number of colums = ";
+    cin >> y;
+    if (x % 2 == 0 || y % 2 == 0)
+    {
+        cout << "Please enter a odd number" << endl;
+    }
+    else 
+    {
+        game mygame(x, y);
+        mygame.display();
+    }
+}
